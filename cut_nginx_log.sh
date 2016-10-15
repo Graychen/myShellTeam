f48d3f5a956203dbfa49406c5e83a98c6edf4aa2
Graychen <13780185250@sina.cn>
2016-10-15 14:41:13 +0800

    完善切割访问日志的shell

--- cut_nginx_log.sh
+++ cut_nginx_log.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+#cut_nxinx access log by graychen  20161025
+#00 00 * * * /home/graychen/myShellTeam/cut_nginx_log.sh > /dev/null 2 >&1
 mv /usr/local/webserver/nginx/logs/www_access.log www_access_$(date +%F -d -1day).log
 /usr/local/webserver/nginx/sbin/nginx -s reload
 
